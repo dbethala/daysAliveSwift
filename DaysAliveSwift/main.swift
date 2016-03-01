ee958e4efdc99882310ddc62a69aa7791f37931c
David Bethala <david.bethala@icloud.com>
2016-02-29 20:50:36 -0500

    Initial Commit

---
+++ DaysAliveSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DaysAliveSwift
+//
+//  Created by David Bethala on 2/29/16.
+//  Copyright © 2016 David Bethala. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
